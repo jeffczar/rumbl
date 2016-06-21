8066f56a23a442f2c451d9523a3a19e21fd0cdf1
Jeffrey Czarnowski <jczar235@gmail.com>
2016-06-21 19:31:40 -0400

    *  First pass at implementing user registration *  Add show, new, and create actions

--- web/controllers/user_controller.ex
+++ web/controllers/user_controller.ex
@@ -3,6 +3,8 @@ defmodule Rumbl.UserController do
 
   alias Rumbl.User
 
+  plug :scrub_params, "user" when action in [:create]
+
   def index(conn, _params) do
     users = Repo.all(User)
     render conn, :index, users: users
@@ -11,5 +13,22 @@ defmodule Rumbl.UserController do
   def show(conn, %{"id" => id}) do
     user = Repo.get(User, id)
     render conn, :show, user: user
+  end
+
+  def new(conn, _params) do
+    changeset = User.changeset(%User{})
+    render conn, :new, changeset: changeset
+  end
+
+  def create(conn, %{"user" => user}) do
+    changeset = User.changeset(%User{}, user)
+    case Repo.insert(changeset) do
+      {:ok, user} ->
+        conn 
+        |> put_flash(:info, "Welcome to Rumbl.io #{user.username}!")
+        |> redirect(to: user_path(conn, :index))
+      {:error, changeset} ->
+        render conn, :new, changeset: changeset
+    end
   end
 end
